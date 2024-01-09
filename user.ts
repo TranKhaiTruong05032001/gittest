a7d5bc00314062da268f41d8d4c821c0946d466b
TruongTran <truongtruong05032001@gmail.com>
2024-01-09 16:22:14 +0700

    commit5

--- user.ts
+++ user.ts
@@ -16,16 +16,16 @@ class User{
     }
 }
 
-let user1 = new User();
-export default user1;
-user1.callElevator(10,['goup',1],'normal');
-setTimeout(() => {
-    user1.callElevator(17,['godown',-1],'normal');   
-}, 3000);
+// let user1 = new User();
+// export default user1;
+// user1.callElevator(10,['goup',1],'normal');
 // setTimeout(() => {
-//     user1.callElevator(12,['goup',1],'vip');
-// }, 2000);
-// user1.callElevator(12,['godown',-1],'vip');
-setTimeout(() => {
-    user1.callElevator(10,['godown',-1],'normal')
-}, 6000);
+//     user1.callElevator(17,['godown',-1],'normal');   
+// }, 3000);
+// // setTimeout(() => {
+// //     user1.callElevator(12,['goup',1],'vip');
+// // }, 2000);
+// // user1.callElevator(12,['godown',-1],'vip');
+// setTimeout(() => {
+//     user1.callElevator(10,['godown',-1],'normal')
+// }, 6000);
